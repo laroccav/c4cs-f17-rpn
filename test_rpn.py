60514a3a4aebbb4422f713780a7a5bbd84a99cd3
Vincent LaRocca <laroccav@umich.edu>
2017-10-27 12:31:51 -0400

    added test cases for all operators

--- test_rpn.py
+++ test_rpn.py
@@ -5,4 +5,20 @@ import rpn
 class TestBasics(unittest.TestCase):
 	def test_add(self):
 		result = rpn.calculate('1 1 +')
-		self.assertEqual(2, result)    
+		self.assertEqual(2, result)   
+	
+	def test_subtract(self):
+		result = rpn.calculate('5 3 -')
+		self.assertEqual(2, result) 
+
+	def test_multi(self):
+		result = rpn.calculate('5 4 *')
+		self.assertEqual(20, result) 
+
+	def test_divide(self):
+		result = rpn.calculate('16 8 /')
+		self.assertEqual(2, result) 
+
+	def test_mode(self):
+		result = rpn.calculate('14 6 %')
+		self.assertEqual(2, result) 
