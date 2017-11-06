15117b705547f394296d200fb96f05df30458728
laroccav <laroccav@umich.edu>
2017-11-06 17:12:48 -0500

    Final fix to travis file

--- test_rpn.py
+++ test_rpn.py
@@ -4,8 +4,8 @@ import rpn
 
 class TestBasics(unittest.TestCase):
 	def test_add(self):
-		result = rpn.calculate('1 1 +')
-		self.assertEqual(2, result)   
+		result = rpn.calculate('-1 1 +')
+		self.assertEqual(0, result)   
 	def test_subtract(self):
 		result = rpn.calculate('5 3 -')
 		self.assertEqual(2, result) 
@@ -19,6 +19,6 @@ class TestBasics(unittest.TestCase):
 	def test_mod(self):
 		result = rpn.calculate('14 6 %')
 		self.assertEqual(2, result) 
-	#def test_exp(self):
-	#	result = rpn.calculate('2 4 ^')
-	#	self.assertEqual(16, result)
+	def test_exp(self):
+		result = rpn.calculate('2 4 ^')
+		self.assertEqual(16, result)
