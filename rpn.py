d33a1bc2a4817603f88d480426c7e9f43998438c
laroccav <laroccav@umich.edu>
2017-11-06 21:17:06 -0500

    Added colorama code to rpn

--- rpn.py
+++ rpn.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 import operator
+import readline
+import colorama
+from colorama import init
+init()
+from colorama import Fore, Back, Style
 
 ops = {
 	'+': operator.add,
@@ -12,6 +17,7 @@ ops = {
 
 def calculate(arg):
 	stack = list()
+	in_string = arg
 	for token in arg.split():
 		try:
 			stack.append(int(token))
@@ -21,11 +27,23 @@ def calculate(arg):
 			function = ops[token]
 			result = function(arg1, arg2)
 			stack.append(result)
+		# print(stack)
+	if len(stack) != 1:
+		raise TypeError("Too many parameters")
+	for token2 in in_string.split():
+		try:
+			if int(token2) < 0:
+				print(Back.RED + token2 + Style.RESET_ALL, end=' ')
+			else:
+				print(token2, end=' ')
+		except ValueError:
+			print(Fore.GREEN + Style.BRIGHT + token2 + Style.RESET_ALL, end=' ')
+	print()
 	return stack.pop()
 
 def main():
     while True:
-        print (calculate(input("rpn calc> ")))
+        print ("Result: ", calculate(input("rpn calc> ")))
 
 if __name__ == '__main__': # Note: that's "underscore underscore n a m e ..."
     main()
--- test_rpn.py
+++ test_rpn.py
@@ -6,11 +6,9 @@ class TestBasics(unittest.TestCase):
 	def test_add(self):
 		result = rpn.calculate('1 1 +')
 		self.assertEqual(2, result)   
-	
 	def test_subtract(self):
 		result = rpn.calculate('5 3 -')
 		self.assertEqual(2, result) 
-
 	def test_multi(self):
 		result = rpn.calculate('5 4 *')
 		self.assertEqual(20, result) 
@@ -18,7 +16,6 @@ class TestBasics(unittest.TestCase):
 	def test_divide(self):
 		result = rpn.calculate('16 8 /')
 		self.assertEqual(2, result) 
-
 	def test_mod(self):
 		result = rpn.calculate('14 6 %')
 		self.assertEqual(2, result) 
